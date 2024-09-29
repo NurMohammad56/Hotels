05d331ca17f8bef5c8f58fa78320362c151976f2
Nur Mohammad <milonsardar56@gmail.com>
2024-09-29 20:58:32 +0600

    Complete create mongoDB databse in Atlas and successfully connected into my project

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const app = express();
-const port = 4000;
+const mongoose = require("mongoose");
+const port = process.env.PORT || 4000;
+
+async function main() {
+  await mongoose.connect(
+    "mongodb+srv://hotels:hotels1221@hotels-cluster.bu0ud.mongodb.net/?retryWrites=true&w=majority&appName=hotels-cluster"
+  );
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+main()
+  .then(() => console.log("Database Connected...."))
+  .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
 app.listen(port, () => {
-  console.log(`Running on ${port}`);
+  console.log(`Running on ${port}...`);
 });
