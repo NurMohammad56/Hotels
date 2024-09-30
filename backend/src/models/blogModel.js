2e1ddb445872deac9ccab98a35f56aa84f773989
Nur Mohammad <milonsardar56@gmail.com>
2024-09-30 22:34:24 +0600

    Blog Model Complete

---
+++ backend/src/models/blogModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const BlogSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+  content: String,
+  coverImg: String,
+  category: String,
+  author: String,
+  rating: Number,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Blog = mongoose.model("Blog", BlogSchema);
+module.exports = Blog;
