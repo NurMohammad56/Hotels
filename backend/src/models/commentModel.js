96542548919c2c8c09ced6091dca52551643b739
Nur Mohammad <milonsardar56@gmail.com>
2024-10-03 13:09:26 +0600

    Created commentModel

---
+++ backend/src/models/commentModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const commentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    required: true,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  postID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Blog",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const comment = mongoose.model("Comment", commentSchema);
+module.exports = comment;
