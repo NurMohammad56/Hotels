74ddc7f48bed316bbfb5f461e6f997286365f76b
Nur Mohammad <milonsardar56@gmail.com>
2024-10-05 22:56:11 +0600

    Create controller for userLogin

--- backend/src/controllers/authUserController.js
+++ backend/src/controllers/authUserController.js
@@ -4,3 +4,7 @@ exports.registerUsers = async (req, res) => {
   let result = await registerUser(req);
   return res.status(200).json(result);
 };
+exports.loginUsers = async (req, res) => {
+  let result = await userLogin(req);
+  return res.status(200).json(result);
+};
