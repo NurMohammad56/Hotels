7e13d3045c7634d3c12a8182673adefe92f69a09
Nur Mohammad <milonsardar56@gmail.com>
2024-10-08 21:00:22 +0600

    Setup full architecture for frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,5 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-950">Hello world!</h1>
+  );
 }
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bgPrimary: "#F7F8F9",
+        primary: "#222222",
+        accent: "#34495E",
+      },
+    },
   },
   plugins: [],
 };
