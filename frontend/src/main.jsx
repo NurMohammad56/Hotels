6a6cb6ca0feda14a8a8ee02c236f1e385ecde201
Nur Mohammad <milonsardar56@gmail.com>
2024-10-10 17:19:39 +0600

    Store and provider complete for blog

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,12 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router/router.jsx";
+import { store } from "./redux/store.jsx";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+ReactDom.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>
+  </Provider>
 );
--- frontend/src/redux/features/blogs/blogsApi.js
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const blogApi = createApi({
   reducerPath: "blogsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4000/api/v1/",
+    baseUrl: "http://localhost:4000/api/v1",
     credentials: "include",
   }),
   endpoints: (builder) => ({
