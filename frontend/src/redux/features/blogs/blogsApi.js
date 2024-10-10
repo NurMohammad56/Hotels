fc0d1411e5b5ce2f1a95a2bd4920fe3091adb0d9
Nur Mohammad <milonsardar56@gmail.com>
2024-10-10 17:01:30 +0600

    createApi for blogApi for redux

---
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const blogApi = createApi({
+  reducerPath: "blogsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:4000/api/v1/",
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    fetchBlogs: builder.query({
+      query: ({ search = "", category = "", location = "" }) =>
+        `/blog?search=${search}&category=${category}$location=${location}`,
+    }),
+  }),
+});
+
+export const { useFetchBlogsQuery } = blogApi;
