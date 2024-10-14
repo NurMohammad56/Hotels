ffcfbf7da890bb0511facc6d3e3efd40b1385e7f
Nur Mohammad <milonsardar56@gmail.com>
2024-10-14 17:35:56 +0600

    working with comment and related blog

--- frontend/src/pages/singleBlog/RelatedBlogs.jsx
+++ frontend/src/pages/singleBlog/RelatedBlogs.jsx
@@ -1,1 +1,31 @@
+import React from "react";
+import { Link, useParams } from "react-router-dom";
+import { useFetchRelatedBlogQuery } from "../../redux/features/blogs/blogsApi";
 
+const RelatedBlogs = () => {
+  const { id } = useParams();
+  const { data: blog = [], error, isLoading } = useFetchRelatedBlogQuery(id);
+  console.log(blog);
+
+  return (
+    <div>
+      <h3 className="text-2xl font-medium pt-6 px-7 pb-4">Related Blogs</h3>
+      <hr />
+      {blog.length > 0 ? (
+        <div className="p-7">No related blogs</div>
+      ) : (
+        <div className="space-y-4 mt-4">
+          {blog.map((blog) => (
+            <Link>
+              <div>
+                <img src={blog.coverImg} alt="" />
+              </div>
+            </Link>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default RelatedBlogs;
