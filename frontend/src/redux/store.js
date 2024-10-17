24b66207d0669ac7b23d0dbb8c7a109d12fdbceb
Nur Mohammad <milonsardar56@gmail.com>
2024-10-17 22:48:09 +0600

    complete store the comment api in redux store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -3,15 +3,21 @@ import { configureStore } from "@reduxjs/toolkit";
 import { blogApi } from "./features/blogs/blogsApi";
 import authApi from "./features/auth/authApi";
 import authReducer from "./features/auth/authSlice";
+import commentApi from "./features/comments/commentApi";
 
 export const store = configureStore({
   reducer: {
     [blogApi.reducerPath]: blogApi.reducer,
     [authApi.reducerPath]: authApi.reducer,
+    [commentApi.reducerPath]: commentApi.reducer,
     auth: authReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(blogApi.middleware, authApi.middleware),
+    getDefaultMiddleware().concat(
+      blogApi.middleware,
+      authApi.middleware,
+      commentApi.middleware
+    ),
 });
 
 export default store;
