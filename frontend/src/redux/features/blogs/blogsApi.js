e77f94fbdad402b8ab3d3aacfbabb9df7fc56b1b
Nur Mohammad <milonsardar56@gmail.com>
2024-10-20 22:11:14 +0600

    Update and Create blog call for redux

--- frontend/src/redux/features/blogs/blogsApi.js
+++ frontend/src/redux/features/blogs/blogsApi.js
@@ -6,16 +6,34 @@ export const blogApi = createApi({
     baseUrl: "http://localhost:4000/api/v1/",
     credentials: "include",
   }),
+  tagTypes: ["Blogs"],
   endpoints: (builder) => ({
     fetchBlogs: builder.query({
       query: ({ search = "", category = "", location = "" }) =>
         `/blog?search=${search}&category=${category}&location=${location}`,
+      providesTags: ["Blogs"],
     }),
     fetchBlogbyId: builder.query({
       query: (id) => `/blog/${id}`,
     }),
     fetchRelatedBlog: builder.query({
       query: (id) => `/related-blog/${id}`,
+    }),
+    postBlog: builder.mutation({
+      query: (newBlog) => ({
+        url: `create-blog`,
+        method: "POST",
+        body: newBlog,
+        credentials: "include",
+      }),
+    }),
+    updateBlog: builder.mutation({
+      query: ({ id, ...rest }) => ({
+        url: `update-blog/${id}`,
+        method: "PATCH",
+        body: rest,
+        credentials: "include",
+      }),
     }),
   }),
 });
