8a8467b599be3e7a82432cf3a851c44d0e8b92f3
Nur Mohammad <milonsardar56@gmail.com>
2024-10-23 23:12:33 +0600

    If role is user then redirect in login page else dashboard completed

--- frontend/src/pages/admin/AdminLayout.jsx
+++ frontend/src/pages/admin/AdminLayout.jsx
@@ -1,8 +1,14 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 import AdminNavigation from "./AdminNavigation";
+import { useSelector } from "react-redux";
 
 const AdminLayout = () => {
+  const { user } = useSelector((state) => state.auth);
+  // console.log(user);
+  if (!user || user.role !== "admin") {
+    return <Navigate to="/login" />;
+  }
   return (
     <div className="container mx-auto flex flex-col md:flex-row gap-4 items-start justify-start mt-6">
       <header className="lg:w-1/5 sm:w-2/5 w-full">
