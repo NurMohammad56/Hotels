49c5602d49bbe98e07c137f157860ed7df0d5061
Nur Mohammad <milonsardar56@gmail.com>
2024-10-31 22:00:20 +0600

    Admin image setup

--- frontend/src/pages/admin/AdminNavigation.jsx
+++ frontend/src/pages/admin/AdminNavigation.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import adminPng from "../../asstes/hero-carosel/admin.png";
 
 const AdminNavigation = () => {
-  return <div>AdminNavigation</div>;
+  return (
+    // Header section
+    <div>
+      <img src={adminPng} alt="" className="size-14" />
+    </div>
+  );
 };
 
 export default AdminNavigation;
