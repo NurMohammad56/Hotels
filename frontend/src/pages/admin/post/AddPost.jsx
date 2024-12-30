e8c4ecd428de7b83ef4e0aae5f1a7fb8c77261dd
NurMohammad56 <nurmohammad0605@gmail.com>
2024-12-30 12:48:06 +0600

    Working with Add Post

--- frontend/src/pages/admin/post/AddPost.jsx
+++ frontend/src/pages/admin/post/AddPost.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddPost = () => {
+  const [title, setTitle] = useState("");
   return (
     <div className="bg-white md:p-7 p-2">
       <h2 className="text-2xl font-semibold">Create A New Blog Post</h2>
@@ -9,10 +10,21 @@ const AddPost = () => {
           <label className="font-semibold text-xl">Blog Title:</label>
           <input
             className="w-full inline-block bg-bgPrimary focus:outline-none px-y py-2"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
             type="text"
             placeholder="Write here..."
             required
           />
+        </div>
+
+        {/* blog details */}
+        <div className="flex flex-col md:flex-row justify-between items-start gap-4">
+          {/* left side */}
+          <div className="border p-5 w-2/3">Left side</div>
+
+          {/* right side */}
+          <div className="border p-5 w-1/3 space-y-5">Right side</div>
         </div>
       </form>
     </div>
