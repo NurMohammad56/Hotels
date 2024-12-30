43017aabe86bea88bf76396d78dff652a5203c3b
NurMohammad56 <nurmohammad0605@gmail.com>
2024-12-30 17:26:48 +0600

    successfully console the data using editor js

--- frontend/src/pages/admin/post/AddPost.jsx
+++ frontend/src/pages/admin/post/AddPost.jsx
@@ -1,7 +1,11 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { useSelector } from "react-redux";
+import EditorJS from "@editorjs/editorjs";
+import List from "@editorjs/list";
+import Header from "@editorjs/header";
 
 const AddPost = () => {
+  const editorRef = useRef(null);
   const [title, setTitle] = useState("");
   const [coverImg, SetcoverImg] = useState("");
   const [metaDes, SetmetaDes] = useState("");
@@ -10,10 +14,46 @@ const AddPost = () => {
   const [message, Setmessage] = useState("");
 
   const { user } = useSelector((state) => state.auth);
+
+  useEffect(() => {
+    const editor = new EditorJS({
+      holder: "editorjs",
+      onReady: () => {
+        editorRef.current = editor;
+      },
+      autofocus: true,
+      tools: {
+        header: {
+          class: Header,
+          inlineToolbar: true,
+        },
+        list: {
+          class: List,
+          inlineToolbar: true,
+        },
+      },
+    });
+    return () => {
+      editor.destroy();
+      editorRef.current = null;
+    };
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const content = await editorRef.current.save();
+      console.log(content);
+    } catch (error) {
+      console.log("Failed to submit post".error);
+    }
+  };
+
   return (
     <div className="bg-white md:p-3 p-2">
       <h2 className="text-2xl font-semibold">Create A New Blog Post</h2>
-      <form className="space-y-5 pt-6">
+      <form onSubmit={handleSubmit} className="space-y-5 pt-6">
         <div className="space-y-3">
           <label className="font-semibold text-xl">Blog Title:</label>
           <input
@@ -29,7 +69,11 @@ const AddPost = () => {
         {/* blog details */}
         <div className="flex flex-col md:flex-row justify-between items-start gap-4">
           {/* left side */}
-          <div className="border p-4 w-2/3">Left side</div>
+          <div className="border p-4 w-2/3">
+            <p className="font-semibold text-lg mb-4">Content Section</p>
+            <p className="text-xs italic">Write your post bellow here</p>
+            <div id="editorjs"></div>
+          </div>
 
           {/* right side */}
           <div className="border p-4 w-1/3 space-y-5">
@@ -101,6 +145,14 @@ const AddPost = () => {
             </div>
           </div>
         </div>
+
+        {message && <p className="text-red-500">{message}</p>}
+        <button
+          className="w-full mt-4 bg-primary hover:bg-indigo-500 text-white font-medium py-1 rounded-md"
+          type="submit"
+        >
+          Add new blog
+        </button>
       </form>
     </div>
   );
