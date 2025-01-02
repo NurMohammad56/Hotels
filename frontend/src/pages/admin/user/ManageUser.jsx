05478ea2c7c44bb28d6c51883c292045c6b02259
NurMohammad56 <nurmohammad0605@gmail.com>
2025-01-02 18:07:27 +0600

    User getting successfully

--- frontend/src/pages/admin/user/ManageUser.jsx
+++ frontend/src/pages/admin/user/ManageUser.jsx
@@ -1,7 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
+import { useGetUserQuery } from "../../../redux/features/auth/authApi";
 
 const ManageUser = () => {
-  return <div>ManageUser</div>;
+  const [selectUser, setSelectUser] = useState(null);
+  const { data: user = [], error, isLoading, refetch } = useGetUserQuery();
+  console.log(user);
+  return (
+    <>
+      {isLoading && <div>Loading....</div>}
+
+      <section className="py-1 bg-blueGray-50">
+        <div className="w-full  mb-12 xl:mb-0 px-4 mx-auto ">
+          <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded ">
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+              <div className="flex flex-wrap items-center">
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                  <h3 className="font-semibold text-base text-blueGray-700">
+                    All Blogs
+                  </h3>
+                </div>
+                <div className="relative w-full px-4 max-w-full flex-grow flex-1 text-right">
+                  <button
+                    className="bg-indigo-500 text-white active:bg-indigo-600 text-xs font-bold uppercase px-3 py-1 rounded outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                    type="button"
+                  >
+                    See all
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div className="block w-full overflow-x-auto">
+              <table className="items-center bg-transparent w-full border-collapse ">
+                <thead>
+                  <tr>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      No.
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Blog name
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Publishing date
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Edit & Manage
+                    </th>
+                    <th className="px-6 bg-blueGray-50 text-blueGray-500 align-middle border border-solid border-blueGray-100 py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left">
+                      Remove
+                    </th>
+                  </tr>
+                </thead>
+
+                <tbody>
+                  {/* {blog &&
+                      blog.map((blogs, index) => (
+                        <tr key={index}>
+                          <th className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left text-blueGray-700 ">
+                            {index + 1}
+                          </th>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-wrap p-4 ">
+                            {blogs.title}
+                          </td>
+                          <td className="border-t-0 px-6 align-center border-l-0 border-r-0 text-xs whitespace-nowrap p-4">
+                            {formateDate(blogs.createdAt)}
+                          </td>
+                          <td className="border-t-0 px-6 align-middle  border-l-0 border-r-0 text-xs whitespace-wrap p-4">
+                            <Link
+                              to={`/dashboard/update-item/${blogs?._id}`}
+                              className="hover:text-blue-700 "
+                            >
+                              <span className="flex gap-1 justify-center items-center">
+                                <MdOutlineEdit /> Edit
+                              </span>
+                            </Link>
+                          </td>
+                          <td className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-wrap p-4">
+                            <button
+                              onClick={() => handleDelete(blogs._id)}
+                              className="bg-red-600 text-white px-2 py-1"
+                            >
+                              Remove
+                            </button>
+                          </td>
+                        </tr>
+                      ))} */}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ManageUser;
